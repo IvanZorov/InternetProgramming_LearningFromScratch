dbf8f72dab031f54706a25051ee7957eea53169d
IvanZorov <i_zoro007@abv.bg>
2014-11-23 17:20:14 +0200

    The begining of the end.

---
+++ Basics_01/FirstStep.java
@@ -0,0 +1,14 @@
+public class FirstStep {
+	public static void main(String[] args) {
+		int i;
+		
+		for (i=1; i<=10; i++) {
+		     if ( i % 2 == 0)
+		     {
+		     System.out.print("Chetno: " + i +"\n");
+		     } else {
+		    	 System.out.println("Nechetno: " + i);
+		     }
+		}
+	}
+}
